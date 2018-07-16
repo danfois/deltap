e236f8c5e9e529a35bbb393ca95ed6ebe7eb5946
danfois <daniele.fois@yesc.it>
2018-07-16 16:00:57 +0200

    Created user creation template. Added method to usercontroller to display the form

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace AppBundle\Controller;
+use AppBundle\Entity\User;
+use AppBundle\Form\CreateUserType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -9,5 +11,16 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController extends Controller
 {
+    /**
+     * @Route("create-user", name="create_user")
+     */
+    public function createUserAction()
+    {
+        $user = new User();
+        $form = $this->createForm(CreateUserType::class, $user);
 
+        return $this->render('users/create_user.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
 }
