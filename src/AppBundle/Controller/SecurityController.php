c5135f38fd33d6d8045781feac9a3c63cd7305c8
danfois <daniele.fois@yesc.it>
2018-07-16 12:50:32 +0200

    Implemented basic symfony login

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request, AuthenticationUtils $authenticationUtils) {
+
+        //utilità di autenticazione predefinite in symfony
+        $authenticationUtils = $this->get('security.authentication_utils');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login.html.twig', array(
+            'error' => $error
+        ));
+    }
+}
