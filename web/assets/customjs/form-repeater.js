4d70ad131bf81c423b8e1834462ff3d060a6f601
danfois <daniele.fois@yesc.it>
2018-07-22 17:49:04 +0200

    Repeater now works better with bootstrap accordion

--- web/assets/customjs/form-repeater.js
+++ web/assets/customjs/form-repeater.js
@@ -2,7 +2,13 @@ var FormRepeater = {
     init: function () {
         $(".repeater").repeater({
             initEmpty: !1, defaultValues: {"text-input": "foo"}, show: function () {
-                $(this).slideDown()
+                $(this).slideDown();
+                var $id = $('.itiner_class').last().attr('id');
+                var $href = $('.itiner_head').last().attr('href');
+                var $title = $('.itiner_title').last().text();
+                $('.itiner_class').last().attr('id', getFirstPart($id, '_') + '_' + (parseInt(getSecondPart($id, '_')) + 1));
+                $('.itiner_head').last().attr('href', getFirstPart($href, '_') + '_' + (parseInt(getSecondPart($href, '_')) + 1));
+                $('.itiner_title').last().text(getFirstPart($title, '#') + '#' + (parseInt(getSecondPart($title, '#')) + 1));
             }, hide: function (e) {
                 $(this).slideUp(e)
             }
@@ -12,33 +18,18 @@ var FormRepeater = {
             }, hide: function (e) {
                 confirm("Are you sure you want to delete this element?") && $(this).slideUp(e)
             }
-        }), $("#m_repeater_3").repeater({
-            initEmpty: !1, defaultValues: {"text-input": "foo"}, show: function () {
-                $(this).slideDown()
-            }, hide: function (e) {
-                confirm("Are you sure you want to delete this element?") && $(this).slideUp(e)
-            }
-        }), $("#m_repeater_4").repeater({
-            initEmpty: !1, defaultValues: {"text-input": "foo"}, show: function () {
-                $(this).slideDown()
-            }, hide: function (e) {
-                $(this).slideUp(e)
-            }
-        }), $("#m_repeater_5").repeater({
-            initEmpty: !1, defaultValues: {"text-input": "foo"}, show: function () {
-                $(this).slideDown()
-            }, hide: function (e) {
-                $(this).slideUp(e)
-            }
-        }), $("#m_repeater_6").repeater({
-            initEmpty: !1, defaultValues: {"text-input": "foo"}, show: function () {
-                $(this).slideDown()
-            }, hide: function (e) {
-                $(this).slideUp(e)
-            }
         })
     }
 };
+
+function getFirstPart(str, sym) {
+    return str.split(sym)[0];
+}
+
+function getSecondPart(str, sym) {
+    return str.split(sym)[1];
+}
+
 jQuery(document).ready(function () {
     FormRepeater.init()
 });
