ac60eaba57a0f195576582014a14a3f6289cff45
danfois <daniele.fois@yesc.it>
2018-07-23 16:22:18 +0200

    Added modals to pricequotations, and refactored modal from create_customer.html.twig

--- src/AppBundle/Controller/PriceQuotationController.php
+++ src/AppBundle/Controller/PriceQuotationController.php
@@ -3,6 +3,9 @@
 namespace AppBundle\Controller;
 use AppBundle\Entity\PriceQuotation;
 use AppBundle\Entity\PriceQuotationDetail;
+use AppBundle\Form\CreateCategoryType;
+use AppBundle\Form\CreateServiceType;
+use AppBundle\Form\CreateServiceTypeType;
 use AppBundle\Form\PriceQuotationType;
 use AppBundle\Form\RepeatedTimesType;
 use AppBundle\Util\TableMaker;
@@ -23,9 +26,15 @@ class PriceQuotationController extends Controller
         $PQ->getQuotationDetails()->add($QD);
         $QD->getArrayRepeatedTimes()->add(new RepeatedTimesType());
         $form = $this->createForm(PriceQuotationType::class, $PQ);
+        $formCategory = $this->createForm(CreateCategoryType::class);
+        $formServiceType = $this->createForm(CreateServiceTypeType::class);
+        $formService = $this->createForm(CreateServiceType::class);
 
         return $this->render('price_quotations/price_quotation.html.twig', array(
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'category_form' => $formCategory->createView(),
+            'service_type_form' => $formServiceType->createView(),
+            'service_form' => $formService->createView()
         ));
     }
 }
