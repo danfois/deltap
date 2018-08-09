5b9f93cfc46b40cc6702054acc8a6b91ac9f5b80
danfois <daniele.fois@yesc.it>
2018-08-09 16:05:58 +0200

    Created base template for vehicle creation. Added basic controller for vehicles, and did some other stuff

---
+++ src/AppBundle/Controller/Vehicle/VehicleController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Controller\Vehicle;
+use AppBundle\Entity\Vehicle\Vehicle;
+use AppBundle\Form\Vehicle\VehicleType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class VehicleController extends Controller
+{
+    /**
+     * @Route("create-vehicle", name="create_vehicle")
+     */
+    public function createVehicleAction()
+    {
+        $vehicle = new Vehicle();
+
+        $form = $this->createForm(VehicleType::class, $vehicle);
+
+        /*return $this->render('DEBUG/show_form.html.twig', array(
+            'form' => $form->createView()
+        ));*/
+
+        return $this->render('vehicles/create_vehicle.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+}
