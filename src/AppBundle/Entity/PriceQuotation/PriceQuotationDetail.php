68de685e0b850e9c1b98ac47bcb1198e6a2032c7
danfois <daniele.fois@yesc.it>
2018-09-01 18:45:26 +0200

    Price Quotation entities have been generated, and database updated

--- src/AppBundle/Entity/PriceQuotation/PriceQuotationDetail.php
+++ src/AppBundle/Entity/PriceQuotation/PriceQuotationDetail.php
@@ -39,17 +39,189 @@ class PriceQuotationDetail
     /**
      * @ORM\Column(type="string", length=64, nullable=false, name="name")
      * @Assert\NotBlank(message="Price Quotation Detail name cannot be null")
-     * @Assert\Length(max=64, maxMessage=Price Quotation Detail name too long. Max 64 chars)
+     * @Assert\Length(max=64, maxMessage="Price Quotation Detail name too long. Max 64 chars")
      */
     protected $name;
 
     /**
-     * @ORM\Column(type="text", nullable="true", name="description")
+     * @ORM\Column(type="text", nullable=true, name="description")
      */
     protected $description;
 
     /**
-     * @ORM\OneToMany(targetEntity="AppBundle\Entity\PriceQuotation\Stages", mappedBy="priceQuotationDetail")
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\PriceQuotation\Stage", mappedBy="priceQuotationDetail")
      */
     protected $stages;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->stages = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get priceQuotationDetailId
+     *
+     * @return integer
+     */
+    public function getPriceQuotationDetailId()
+    {
+        return $this->priceQuotationDetailId;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return PriceQuotationDetail
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return PriceQuotationDetail
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set priceQuotation
+     *
+     * @param \AppBundle\Entity\PriceQuotation\PriceQuotation $priceQuotation
+     *
+     * @return PriceQuotationDetail
+     */
+    public function setPriceQuotation(\AppBundle\Entity\PriceQuotation\PriceQuotation $priceQuotation = null)
+    {
+        $this->priceQuotation = $priceQuotation;
+
+        return $this;
+    }
+
+    /**
+     * Get priceQuotation
+     *
+     * @return \AppBundle\Entity\PriceQuotation\PriceQuotation
+     */
+    public function getPriceQuotation()
+    {
+        return $this->priceQuotation;
+    }
+
+    /**
+     * Set serviceType
+     *
+     * @param \AppBundle\Entity\ServiceType $serviceType
+     *
+     * @return PriceQuotationDetail
+     */
+    public function setServiceType(\AppBundle\Entity\ServiceType $serviceType = null)
+    {
+        $this->serviceType = $serviceType;
+
+        return $this;
+    }
+
+    /**
+     * Get serviceType
+     *
+     * @return \AppBundle\Entity\ServiceType
+     */
+    public function getServiceType()
+    {
+        return $this->serviceType;
+    }
+
+    /**
+     * Set serviceCode
+     *
+     * @param \AppBundle\Entity\Service $serviceCode
+     *
+     * @return PriceQuotationDetail
+     */
+    public function setServiceCode(\AppBundle\Entity\Service $serviceCode = null)
+    {
+        $this->serviceCode = $serviceCode;
+
+        return $this;
+    }
+
+    /**
+     * Get serviceCode
+     *
+     * @return \AppBundle\Entity\Service
+     */
+    public function getServiceCode()
+    {
+        return $this->serviceCode;
+    }
+
+    /**
+     * Add stage
+     *
+     * @param \AppBundle\Entity\PriceQuotation\Stage $stage
+     *
+     * @return PriceQuotationDetail
+     */
+    public function addStage(\AppBundle\Entity\PriceQuotation\Stage $stage)
+    {
+        $this->stages[] = $stage;
+
+        return $this;
+    }
+
+    /**
+     * Remove stage
+     *
+     * @param \AppBundle\Entity\PriceQuotation\Stage $stage
+     */
+    public function removeStage(\AppBundle\Entity\PriceQuotation\Stage $stage)
+    {
+        $this->stages->removeElement($stage);
+    }
+
+    /**
+     * Get stages
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getStages()
+    {
+        return $this->stages;
+    }
 }
--- src/AppBundle/Entity/PriceQuotation/Stage.php
+++ src/AppBundle/Entity/PriceQuotation/Stage.php
@@ -2,7 +2,7 @@
 
 namespace AppBundle\Entity\PriceQuotation;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraint as Assert;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="AppBundle\Repository\StageRepository")
@@ -83,4 +83,279 @@ class Stage
      * @Assert\NotBlank(message="Estimated time for a stage cannot be null")
      */
     protected $estimatedTime;
+
+
+    /**
+     * Get stageId
+     *
+     * @return integer
+     */
+    public function getStageId()
+    {
+        return $this->stageId;
+    }
+
+    /**
+     * Set departureLocation
+     *
+     * @param string $departureLocation
+     *
+     * @return Stage
+     */
+    public function setDepartureLocation($departureLocation)
+    {
+        $this->departureLocation = $departureLocation;
+
+        return $this;
+    }
+
+    /**
+     * Get departureLocation
+     *
+     * @return string
+     */
+    public function getDepartureLocation()
+    {
+        return $this->departureLocation;
+    }
+
+    /**
+     * Set departureArrival
+     *
+     * @param string $departureArrival
+     *
+     * @return Stage
+     */
+    public function setDepartureArrival($departureArrival)
+    {
+        $this->departureArrival = $departureArrival;
+
+        return $this;
+    }
+
+    /**
+     * Get departureArrival
+     *
+     * @return string
+     */
+    public function getDepartureArrival()
+    {
+        return $this->departureArrival;
+    }
+
+    /**
+     * Set departureDate
+     *
+     * @param \DateTime $departureDate
+     *
+     * @return Stage
+     */
+    public function setDepartureDate($departureDate)
+    {
+        $this->departureDate = $departureDate;
+
+        return $this;
+    }
+
+    /**
+     * Get departureDate
+     *
+     * @return \DateTime
+     */
+    public function getDepartureDate()
+    {
+        return $this->departureDate;
+    }
+
+    /**
+     * Set arrivalDate
+     *
+     * @param \DateTime $arrivalDate
+     *
+     * @return Stage
+     */
+    public function setArrivalDate($arrivalDate)
+    {
+        $this->arrivalDate = $arrivalDate;
+
+        return $this;
+    }
+
+    /**
+     * Get arrivalDate
+     *
+     * @return \DateTime
+     */
+    public function getArrivalDate()
+    {
+        return $this->arrivalDate;
+    }
+
+    /**
+     * Set busNumber
+     *
+     * @param integer $busNumber
+     *
+     * @return Stage
+     */
+    public function setBusNumber($busNumber)
+    {
+        $this->busNumber = $busNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get busNumber
+     *
+     * @return integer
+     */
+    public function getBusNumber()
+    {
+        return $this->busNumber;
+    }
+
+    /**
+     * Set km
+     *
+     * @param string $km
+     *
+     * @return Stage
+     */
+    public function setKm($km)
+    {
+        $this->km = $km;
+
+        return $this;
+    }
+
+    /**
+     * Get km
+     *
+     * @return string
+     */
+    public function getKm()
+    {
+        return $this->km;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     *
+     * @return Stage
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set repeatedTimes
+     *
+     * @param array $repeatedTimes
+     *
+     * @return Stage
+     */
+    public function setRepeatedTimes($repeatedTimes)
+    {
+        $this->repeatedTimes = $repeatedTimes;
+
+        return $this;
+    }
+
+    /**
+     * Get repeatedTimes
+     *
+     * @return array
+     */
+    public function getRepeatedTimes()
+    {
+        return $this->repeatedTimes;
+    }
+
+    /**
+     * Set repeatedDays
+     *
+     * @param array $repeatedDays
+     *
+     * @return Stage
+     */
+    public function setRepeatedDays($repeatedDays)
+    {
+        $this->repeatedDays = $repeatedDays;
+
+        return $this;
+    }
+
+    /**
+     * Get repeatedDays
+     *
+     * @return array
+     */
+    public function getRepeatedDays()
+    {
+        return $this->repeatedDays;
+    }
+
+    /**
+     * Set estimatedTime
+     *
+     * @param string $estimatedTime
+     *
+     * @return Stage
+     */
+    public function setEstimatedTime($estimatedTime)
+    {
+        $this->estimatedTime = $estimatedTime;
+
+        return $this;
+    }
+
+    /**
+     * Get estimatedTime
+     *
+     * @return string
+     */
+    public function getEstimatedTime()
+    {
+        return $this->estimatedTime;
+    }
+
+    /**
+     * Set priceQuotationDetail
+     *
+     * @param \AppBundle\Entity\PriceQuotation\PriceQuotationDetail $priceQuotationDetail
+     *
+     * @return Stage
+     */
+    public function setPriceQuotationDetail(\AppBundle\Entity\PriceQuotation\PriceQuotationDetail $priceQuotationDetail = null)
+    {
+        $this->priceQuotationDetail = $priceQuotationDetail;
+
+        return $this;
+    }
+
+    /**
+     * Get priceQuotationDetail
+     *
+     * @return \AppBundle\Entity\PriceQuotation\PriceQuotationDetail
+     */
+    public function getPriceQuotationDetail()
+    {
+        return $this->priceQuotationDetail;
+    }
 }
