6bddf56ce980804af2a78f4977ab67e1dad47259
danfois <daniele.fois@yesc.it>
2018-10-20 18:37:16 +0200

    Bank Account editing is now working. javascript has been edited as well

--- src/AppBundle/Controller/Payment/BankAccountController.php
+++ src/AppBundle/Controller/Payment/BankAccountController.php
@@ -60,4 +60,68 @@ class BankAccountController extends Controller
 
         throw new AccessDeniedException('Accesso Negato');
     }
+
+    /**
+     * @Route("edit-bank-account", name="edit_bank_account")
+     */
+    public function editBankAccount(Request $request)
+    {
+        $id = $request->query->get('id');
+        if(is_numeric($id) === false) return new Response('Richiesta effettuata in maniera non corretta o conto corrente non trovato', 400);
+
+        $ba = $this->getDoctrine()->getRepository(BankAccount::class)->findOneBy(array('bankAccountId' => $id));
+        if($ba == null) return new Response('Conto Corrente non trovato', 404);
+
+        $form = $this->createForm(BankAccountType::class, $ba);
+
+        $actionUrl = $this->generateUrl('ajax_edit_bank_account', array('n' => $id));
+
+        $html = $this->renderView('payments/forms/bank_account_form.html.twig', array(
+            'form' => $form->createView(),
+            'action_url' => $actionUrl,
+            'edit' => 'true'
+        ));
+
+        return $this->render('includes/generic_modal_content.html.twig', array(
+            'modal_title' => 'Modifica Conto Corrente',
+            'modal_content' => $html
+        ));
+    }
+
+    /**
+     * @Route("ajax-edit-bank-account-{n}", name="ajax_edit_bank_account")
+     */
+    public function ajaxEditBankAccount(Request $request, int $n)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $ba = $em->getRepository(BankAccount::class)->findOneBy(array('bankAccountId' => $n));
+
+        if($ba == null) return new Response('Conto Corrente non trovato', 404);
+
+        $form = $this->createForm(BankAccountType::class, $ba);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $ba = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+
+            $em->flush();
+
+            return new Response('Conto Corrente modificato con successo', 200);
+        }
+
+        if ($form->isSubmitted() && !$form->isValid()) {
+            $errors = $form->getErrors(true);
+            $error = '';
+
+            foreach($errors as $k => $e) {
+                $error .= $e->getMessage() . '<br> ';
+
+            }
+            return new Response($error, 500);
+        }
+
+        throw new AccessDeniedException('Accesso Negato');
+    }
 }
--- web/assets/customjs/payments/bank_accounts_list.js
+++ web/assets/customjs/payments/bank_accounts_list.js
@@ -66,18 +66,15 @@ var BankAccountList = function () {
                                 <i class="la la-ellipsis-h"></i>\
                             </a>\
 						  	<div class="dropdown-menu dropdown-menu-right">\
-						    	<a class="dropdown-item" href="edit-issued-invoice-' + row.idv +'" onclick=""><i class="la la-edit"></i> Modifica Fattura</a>\
-						    	<a class="dropdown-item" href="javascript:void(0);" onclick="alert(\'In Lavorazione\')"><i class="la la-eye"></i> Vedi Fattura</a>\
-						    	<a class="dropdown-item" href="javascript:void(0);" onclick="alert(\'In Lavorazione\')"><i class="la la-plus-circle"></i> Invia al Cliente</a>\
-						    	\<a class="dropdown-item" href="javascript:void(0);" onclick="alert(\'In Lavorazione\')"><i class="la la-money"></i> Registra Pagamento</a>\
-						    	<a class="dropdown-item" href="javascript:void(0);" onclick="alert(\'In Lavorazione\')"><i class="la la-plus-circle"></i> Inserisci in Prima Nota</a>\
+						    	<a class="dropdown-item" href="javascript:void(0);" onclick="genericModalFunction(\'GET\', \'edit-bank-account\', {\'id\': \'' + row.id + '\'}, {\'initializeWidgets\' : true, \'initializeForm\' : true, \'formJquery\' : \'form_bank_account_edit\' });"><i class="la la-edit"></i> Modifica Conto Corrente</a>\
+						    	<a class="dropdown-item" href="javascript:void(0);" onclick="alert(\'In Lavorazione\')"><i class="la la-print"></i> Stampa PDF Conto Corrente</a>\
 						  	</div>\
 						</div>\
-						<a href="edit-issued-invoice-' + row.idv +'" onclick="" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" title="Modifica Fattura">\
+						<a href="javascript:void(0);" onclick="genericModalFunction(\'GET\', \'edit-bank-account\', {\'id\': \'' + row.id + '\'}, {\'initializeWidgets\' : true, \'initializeForm\' : true, \'formJquery\' : \'form_bank_account_edit\' });" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" title="Modifica Conto Corrente">\
 							<i class="la la-edit"></i>\
 						</a>\
-						<a href="javascript:void(0);" onclick="alert(\'In Lavorazione\')" class="m-portlet__nav-link btn m-btn m-btn--hover-danger m-btn--icon m-btn--icon-only m-btn--pill" title="Vedi Fattura">\
-							<i class="la la-eye"></i>\
+						<a href="javascript:void(0);" onclick="alert(\'In Lavorazione\')" class="m-portlet__nav-link btn m-btn m-btn--hover-danger m-btn--icon m-btn--icon-only m-btn--pill" title="Stampa PDF Dati Conto Corrente">\
+							<i class="la la-print"></i>\
 						</a>\
 					';
                     }
