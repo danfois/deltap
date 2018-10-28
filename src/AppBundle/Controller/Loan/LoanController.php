6b8801755ab12bab0140fb51db6bb35798f759e5
danfois <daniele.fois@yesc.it>
2018-10-28 18:29:54 +0100

    Loan editing is now working

--- src/AppBundle/Controller/Loan/LoanController.php
+++ src/AppBundle/Controller/Loan/LoanController.php
@@ -67,4 +67,60 @@ class LoanController extends Controller
 
         throw new AccessDeniedException('Accesso Negato');
     }
+
+    /**
+     * @Route("edit-loan-{n}", name="edit_loan")
+     */
+    public function editLoanAction(int $n)
+    {
+        $loan = $this->getDoctrine()->getRepository(Loan::class)->find($n);
+        if($loan == null) return new Response('Mutuo non trovato', 404);
+
+        $form = $this->createForm(LoanType::class, $loan);
+
+        return $this->render('loans/loan.html.twig', array(
+            'form' => $form->createView(),
+            'title' => 'Modifica Mutuo - ' . $loan->getLoanNumber(),
+            'action_url' => $this->generateUrl('ajax_edit_loan', array('n' => $n))
+        ));
+    }
+
+    /**
+     * @Route("ajax/edit-loan-{n}", name="ajax_edit_loan")
+     */
+    public function ajaxEditLoanAction(Request $request, int $n)
+    {
+        $loan = $this->getDoctrine()->getRepository(Loan::class)->find($n);
+        if($loan == null) return new Response('Mutuo non trovato', 404);
+
+        $form = $this->createForm(LoanType::class, $loan);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $loan = $form->getData();
+
+            foreach($loan->getLoanInstalments() as $l) {
+                $l->setLoan($loan);
+            }
+
+            $em->flush();
+
+            return new Response('Mutuo Modificato con successo!', 200);
+        }
+
+        if ($form->isSubmitted() && !$form->isValid()) {
+            $errors = $form->getErrors(true);
+            $error = '';
+
+            foreach($errors as $k => $e) {
+                $error .= $e->getMessage() . '<br> ';
+
+            }
+            return new Response($error, 500);
+        }
+
+        throw new AccessDeniedException('Accesso Negato');
+    }
 }
