628a2eec80c281e2bc9a519628fe9e0ed0a65ace
danfois <daniele.fois@yesc.it>
2019-04-13 16:53:32 +0200

    fixed link in received invoice list

--- web/assets/customjs/invoices/receivedInvoiceList.js
+++ web/assets/customjs/invoices/receivedInvoiceList.js
@@ -325,7 +325,7 @@ var InvoiceList = function () {
                             </a>\
 						  	<div class="dropdown-menu dropdown-menu-right">\
 						    	<a class="dropdown-item" href="edit-received-invoice-' + row.idv +'" onclick=""><i class="la la-edit"></i> Modifica Fattura</a>\
-						    	<a class="dropdown-item" href="javascript:void(0);" onclick="alert(\'In Lavorazione\')"><i class="la la-eye"></i> Vedi Fattura</a>\
+						    	<a class="dropdown-item" href="print/received-invoice-' + row.idv + '"><i class="la la-eye"></i> Vedi Fattura</a>\
 						    	\<a class="dropdown-item" href="create-payment-from/receivedInvoice/' + row.idv + '" onclick="" target="_blank"><i class="la la-money"></i> Registra Pagamento</a>\
 						    	<a class="dropdown-item" href="javascript:void(0);" onclick="alert(\'In Lavorazione\')"><i class="la la-plus-circle"></i> Inserisci in Prima Nota</a>\
 						  	</div>\
